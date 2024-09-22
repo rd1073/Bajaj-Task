e924d1adff68c9bf85142f48bcd1e30e60cfc138
rd1073 <rd1073@srmist.edu.in>
2024-09-22 17:50:45 +0530

    yyy

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/bfhl', bfhlRoute);
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
